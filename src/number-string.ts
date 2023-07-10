c8d2deeacf0e421cc2ef414bb3dad01e8ff8b7ee
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-11 08:00:24 +0900

    number/string型

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {}
+
+let year: number = 2000;
+// year = false; // エラー number型以外の値を代入しようとしている
+console.log({ year });
+
+let age: number = 0x2b;
+
+console.log({ age });
+
+let name: string = 'TypeScript';
+
+console.log({ name });
