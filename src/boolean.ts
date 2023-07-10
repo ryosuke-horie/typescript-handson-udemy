49f7ceeb8aea0cecfbab35d4d157994c525797f7
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-11 07:55:19 +0900

    boolean

--- src/boolean.ts
+++ src/boolean.ts
@@ -1,1 +1,16 @@
-export {};
+export {}; // moduleにする。空のオブジェクトをexportすることで、module化することができる
+
+let name = 'TypeScript';
+
+console.log({ name });
+
+// Boolean 型注釈
+let isFinished: boolean = true;
+
+isFinished = false;
+
+// isFinished = 1; // エラー boolean型以外の値を代入しようとしている
+// JSだとエラーにならないが、TSだとエラーになる
+// ＊JSは代入のたびに型を変更できる
+
+console.log({ isFinished });
